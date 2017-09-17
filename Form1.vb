51f4f4ef3ef7ba8b629ace6aabd32aacacea6fb7
RandomWolf <31776319+RandomWolf@users.noreply.github.com>
2017-09-17 18:51:43 +0200

    Sachen

--- Form1.vb
+++ Form1.vb
@@ -92,10 +92,7 @@ Public Class Form1
     End Sub
 
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
-
+    
     End Sub
 
-    Public Sub CheckChecker()
-
-    End Sub
 End Class
